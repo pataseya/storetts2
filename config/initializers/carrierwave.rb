0477e487a1a92597320be03bb161a0d051f7e3ad
PAT CAMP <PATS0825@MSN.COM>
2018-12-15 16:17:45 -0500

    updated uploaded with credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.storage    = :aws
-  config.aws_bucket = ENV.fetch('aws_bucket')
+  config.aws_bucket = "#{ENV['S3_BUCKET_NAME']}"
   config.aws_acl    = 'public-read'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
   config.aws_credentials = {
-    access_key_id:     ENV.fetch('aws_access_key_id'),
-    secret_access_key: ENV.fetch('aws_secret_access_key'),
-    region:            ENV.fetch('aws_region') # Required
+    :aws_access_key_id     => "#{ENV['AWS_ACCESS_KEY_ID']}",
+    :aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
+    :region                => "#{ENV['S3_REGION']}",
   }
 end
