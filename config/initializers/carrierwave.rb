d918a76ea4eef914a5e851df438dcf7fa3f4e272
PAT CAMP <PATS0825@MSN.COM>
2018-12-17 20:56:29 -0500

    updated initalizer carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,15 @@
+
+
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region:                ENV['S3_REGION'],
+    :provider => 'AWS',
+    :aws_access_key_id => "#{ENV['AWS_ACCESS_KEY_ID']}",
+    :aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
+    :region => "#{ENV['S3_REGION']}",
+    :path_style => true
   }
 
-  config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
-  config.cache_dir     = "#{Rails.root}/tmp/uploads"  
+  config.fog_directory = "#{ENV["S3_BUCKET_NAME"]}"
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+
 end
